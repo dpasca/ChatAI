67e25efac4d76c72363a37b7d9f24ad0e06415fc
Davide Pasca <dpasca@gmail.com>
2024-02-10 18:14:00 +0900

    Add print_trimmed and logwarn functions to logger.py

--- app_web/Common/logger.py
+++ app_web/Common/logger.py
@@ -3,12 +3,24 @@ import os
 
 # Enable for debugging purposes
 ENABLE_LOGGING = os.getenv('FORCE_ENABLE_LOGGING', '0') == '1'
+VERBOSE_LOGGING = os.getenv('VERBOSE_LOGGING', '0') == '1'
 
 #==================================================================
+def print_trimmed(msg):
+    if len(msg) >150:
+        print(msg[:150] + "...")
+    else:
+        print(msg)
+
 def logmsg(msg):
     if ENABLE_LOGGING:
         caller = inspect.currentframe().f_back.f_code.co_name
-        print(f"[{caller}] {msg}")
+        print_trimmed(f"[{caller}] {msg}")
+
+def logwarn(msg):
+    if ENABLE_LOGGING:
+        caller = inspect.currentframe().f_back.f_code.co_name
+        print(f"\033[93m[WARN]\033[0m[{caller}] {msg}")
 
 def logerr(msg):
     if ENABLE_LOGGING:
